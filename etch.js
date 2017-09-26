52bcfcd991ee9b45b7968da15427da30fe46267a
Malcolm Groves <macgroves@gmail.com>
2017-09-26 18:40:06 -0400

    Debugged hover coloring, added new-row functionality

--- etch.js
+++ etch.js
@@ -1,28 +1,48 @@
 // Fill the container with 100 boxes
-function new_grid() {
-  for(i = 1; i <= 100; i++) {
+function create_grid(rows) {
+  for(i = 1; i <= rows * rows; i++) {
     var grid = $("<div class='grid'></div>");
+
+    $("#container").append(grid);
+
 
     // if(i % 10 == 0) {
     //   grid.addClass("#end");
     // }
-    console.log("i'm in")
-    $("#container").append(grid);
+
+
   }
+
+  $('.grid').width(960/rows);
+  $('.grid').height(960/rows);
 }
 
-function clear_grid() {
-  while($(".grid") != null) {
-    $(".grid").remove();
-  }
-}
+// function clear_grid() {
+//   while($(".grid") != null) {
+//     $(".grid").remove();
+//   }
+// }
 
-function change_grid_color() {
-  $(this).
+function color(element, color) {
+  $(element).css('background-color', color);
 }
 
 $(document).ready(function () {
-  $(".new_grid_button").on('click', new_grid);
-  $(".grid").on('mouseenter', change_grid_color);
+  // $(".new_grid_button").on('click', new_grid);
+  create_grid(10);
+
+  $(".grid").mouseover( function() {
+    color(this, 'slateblue');
+  });
+
+  $(".new_grid_button").click( function() {
+    $('.grid').remove();
+    var rows = prompt("How many rows do you want?")
+    create_grid(rows);
+
+    $(".grid").mouseover( function() {
+      color(this, 'slateblue');
+    });
+  })
   //new_grid();
 });
